a7ca9f1cca0ca79667c9b10a2f1adfc8eb956c8b
Fredy Mendoza Vargas <fredymv03@gmail.com>
2018-06-27 12:05:07 -0500

    Ejercicio2 finalizado

---
+++ ejercicio2/file_worker.py
@@ -0,0 +1,27 @@
+import json
+
+from connection import PikaConnection
+
+
+def callback(ch, method, properties, body):
+    data = json.loads(str(body)[2:-1:])
+    print('[*] Message for broker {} : {}'.format(queue_name, body))
+    with open('logs.txt', 'a') as file:
+        message = '[{}] "{}" {}'.format(data['type'], data['code'], data['body'])
+        file.write('{}\n'.format(message))
+
+
+if __name__ == '__main__':
+    connection = PikaConnection()
+    channel = connection.channel
+
+    channel.exchange_declare(exchange='logs', exchange_type='fanout')
+
+    results = channel.queue_declare(exclusive=True)
+    queue_name = results.method.queue
+    channel.queue_bind(exchange='logs', queue=queue_name)
+
+    print('[*] Starting worker with queue {}'.format(queue_name))
+
+    channel.basic_consume(callback, queue=queue_name, no_ack=True)
+    channel.start_consuming()
---
+++ ejercicio2/publisher.py
@@ -0,0 +1,34 @@
+import json
+import time
+
+from connection import PikaConnection
+
+
+def errors():
+    return [
+        {'type': 'DEBUG', 'code': '123', 'body': 'Error de debug'},
+        {'type': 'WARRNING', 'code': '234', 'body': 'Error de debug'},
+        {'type': 'INFO', 'code': '200', 'body': 'Error de debug'},
+        {'type': 'INFO', 'code': '200', 'body': 'Nueva Informacion'},
+        {'type': 'ERROR', 'code': '500', 'body': 'Ha ocurrido un error en el servidor .......'},
+        {'type': 'ERROR', 'code': '500', 'body': 'Ha ocurrido un 2° error en el servidor .......'},
+        {'type': 'DEBUG', 'code': '123', 'body': 'Error de debug'},
+    ]
+
+
+if __name__ == '__main__':
+    connection = PikaConnection()
+    connection.channel.exchange_declare(exchange='logs', exchange_type='fanout')
+
+    for error in errors():
+        message = json.dumps(error)
+        connection.channel.basic_publish(
+            exchange='logs',
+            routing_key='',
+            body=message
+        )
+        print("[*] Sent message : {}".format(message))
+        time.sleep(3)
+
+    # Close Connection
+    connection.close()
